801fb05d1680c650ba60a635edd13725b167cbbb
Ojasvi Mittal <iamcarlover6505@gmail.com>
2024-05-09 20:06:20 +0530

    commit

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -1,19 +1,4 @@
-/*!
 
- =========================================================
- * Paper Kit - v2.2.0
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/paper-kit
- * Copyright 2019 Creative Tim (http://www.creative-tim.com)
-
- * Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
 
 
 var transparent = true;
